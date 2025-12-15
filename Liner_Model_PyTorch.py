ecefd71eeeb731d40ad97cef600c7eea2d7e6574
weiyicong <257535642@qq.com>
2025-12-15 15:38:31 +0800

    PyTorch实现线性模型

--- Liner_Model_PyTorch.py
+++ Liner_Model_PyTorch.py
@@ -1,9 +1,34 @@
 import torch
 
-x_data=torch.Tensor([1.0],[2.0],[3.0])
-y_data=torch.Tensor([2.0],[4.0],[6.0])
+x_data=torch.Tensor([[1.0],[2.0],[3.0]])
+y_data=torch.Tensor([[2.0],[4.0],[6.0]])
 
-class LinerModel(torch.nn.module):
+class LinerModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.linear=torch.nn.Linear(1,1)
+
+    def forward(self,x):
+        y_pred=self.linear(x)
+        return y_pred
+
+model=LinerModel()
+
+criterion=torch.nn.MSELoss(reduction="sum")
+optimizer=torch.optim.SGD(model.parameters(),lr=0.01)
+
+for epoch in range(1000):
+    y_pred=model(x_data)
+    loss=criterion(y_pred,y_data)
+    print(epoch,loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print('w=',model.linear.weight.item())
+print('b=',model.linear.bias.item())
+
+x_test=torch.Tensor([4.0])
+y_test=model(x_test)
+print('y_pred=',y_test.item())
